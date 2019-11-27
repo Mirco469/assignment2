0fe19c4c1bb5537d2e8e5ff134741f072d396893
Mirco <giardinamirco@gmail.com>
2019-11-27 19:59:54 +0100

    Fatto passare test per issue 4
    close #4

--- src/test/java/it/unipd/tos/business/BillTest.java
+++ src/test/java/it/unipd/tos/business/BillTest.java
@@ -16,12 +16,14 @@ public class BillTest {
     
     Bill conto;
     List<MenuItem> listaProdotti;
+    double tot;
     
     @Before
     public void before()
     {
         conto = new Bill();
         listaProdotti = new ArrayList<MenuItem>();
+        tot = 0;
     }
     
     // Issue 1
@@ -31,7 +33,7 @@ public class BillTest {
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "Patatine", 2.71));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "Acqua", 1.41));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "Gamberi", 5));
-        double tot = conto.getOrderPrice(listaProdotti);
+        tot = conto.getOrderPrice(listaProdotti);
         assertEquals(12.24, tot,0.0);
     }
     
@@ -45,7 +47,7 @@ public class BillTest {
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "No", 3.41));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Invarianti", 7.41));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Please", 3.71));
-        double tot = conto.getOrderPrice(listaProdotti);
+        tot = conto.getOrderPrice(listaProdotti);
         //21.78 - 0.71
         assertEquals(21.07, tot,0.0);
     }
@@ -63,7 +65,7 @@ public class BillTest {
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "Please", 6));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "Please", 7));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "No", 9));
-        double tot = conto.getOrderPrice(listaProdotti);
+        tot = conto.getOrderPrice(listaProdotti);
         //65 - 65*0.1 (lo sconto del 10% si applica a tutto il totale)
         assertEquals(58.5, tot,0.0);
     }
@@ -80,7 +82,7 @@ public class BillTest {
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "Please", 6));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "Please", 7));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "No", 9));
-        double tot = conto.getOrderPrice(listaProdotti);
+        tot = conto.getOrderPrice(listaProdotti);
         //34 euro di cibo, 31 euro di bevande = 65 (niente sconto)
         assertEquals(65, tot,0.0);
     }
@@ -91,7 +93,7 @@ public class BillTest {
     {
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Filè", 4));
         listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "is", 5));
-        double tot = conto.getOrderPrice(listaProdotti);
+        tot = conto.getOrderPrice(listaProdotti);
         //Commissione di 0.5
         assertEquals(9.5, tot,0.0);
     }
@@ -101,7 +103,7 @@ public class BillTest {
     public ExpectedException error= ExpectedException.none();
     
     @Test
-    public void testNonPiuDi30Elementi()
+    public void testNonPiuDi30Elementi() throws TakeAwayBillException
     {
         error.expect(TakeAwayBillException.class);
         error.expectMessage("Errore, ordine contiene più di 30 elementi");
@@ -110,6 +112,7 @@ public class BillTest {
         {
             listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "Filè"+i, 4));
         }
+        tot = conto.getOrderPrice(listaProdotti);
         //Mai raggiunta
     }
 }
