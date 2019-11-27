913a315818c2c740723d4bc0e2a1c3356445fb94
Mirco <giardinamirco@gmail.com>
2019-11-27 16:56:52 +0100

    Tolto altro tab

--- src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -6,6 +6,6 @@ package it.unipd.tos.business.exception;
 public class TakeAwayBillException extends Exception{
     public TakeAwayBillException(String message)
     {
-	    super(message);
+        super(message);
     }
 }
