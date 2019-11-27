08dc65ba39a06b425bb9a7dc8b62e5761d45e440
Mirco <giardinamirco@gmail.com>
2019-11-27 18:34:00 +0100

    Fatto test per issue due

--- src/test/java/it/unipd/tos/business/BillTest.java
+++ src/test/java/it/unipd/tos/business/BillTest.java
@@ -2,6 +2,7 @@ package it.unipd.tos.business;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import it.unipd.tos.business.exception.TakeAwayBillException;
@@ -11,16 +12,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BillTest {
-
+    
+    Bill conto;
+    List<MenuItem> listaProdotti;
+    
+    @Before
+    public void before()
+    {
+        conto = new Bill();
+        listaProdotti = new ArrayList<MenuItem>();
+    }
+    
     @Test
     public void CalcoloTotaleContoConAlcuniProdotti() throws TakeAwayBillException {
-        Bill conto = new Bill();
-        List<MenuItem> listaProdotti = new ArrayList<MenuItem>();
-        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Filè", 3.12));
-        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "is", 2.71));
-        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "numberOne", 1.41));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Primavera", 3.12));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Fritti, "Patatine", 2.71));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Bevande, "Acqua", 1.41));
         double tot = conto.getOrderPrice(listaProdotti);
         assertEquals(7.24, tot,0.0);
     }
-
+    
+    @Test
+    public void ScontoSulPaninoMenoCaroSeOrdinatiPiuDiCinquePanini() throws TakeAwayBillException
+    {
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Filè", 3.12));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "is", 2.71));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "numberOne", 1.42));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "No", 3.41));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Invarianti", 7.41));
+        listaProdotti.add(new MenuItem(MenuItem.Prodotti.Panini, "Please", 3.71));
+        double tot = conto.getOrderPrice(listaProdotti);
+        //22.78 - 1.42/2
+        assertEquals(21.07, tot,0.0);
+    }
 }
